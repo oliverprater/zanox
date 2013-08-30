eeb45c210208970c365797465511874d8538f5e5
Oliver Prater <oliver.prater@gmail.com>
2013-08-30 20:59:12 +0200

    Remove ruby 1.8.x stuff

--- examples/profile.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__),"..","lib","zanox.rb")
-
-# fake session
-Zanox::API::Session.connect_id = 'your connect id'
-Zanox::API::Session.secret_key = 'your secret key'
-
-profile = Zanox::Profile.find()[0]
-
-puts "Hi #{profile['firstName']}!"
